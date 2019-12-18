da32888e5201d05560197671622990f8f1098e10
kaysonwu <772663139@qq.com>
2019-12-19 00:28:44 +0800

    support laravel

---
+++ src/OsserviceProvider.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace League\Flysystem\Aliyun;
+
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Support\ServiceProvider;
+use League\Flysystem\Filesystem;
+use OSS\OssClient;
+
+class OsserviceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        Storage::extend('aliyun-oss', function ($app, $config) {
+
+            $client = @new OssClient(
+                $config['key'],
+                $config['secret'],
+                $config['endpoint'],
+                $config['cname'],
+                $config['token']
+            );
+
+            $prefix = isset($config['prefix']) ? $config['prefix'] : '';
+            $options = isset($config['options']) ? $config['options'] : [];
+
+            return new Filesystem(new OssAdapter($client, $config['bucket'], $prefix, $options));
+        });
+    }
+}
